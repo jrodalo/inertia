dfcce684fd46cc93af31d662774312955b96aa5d
Jose Rodriguez <jrodalo@leanmind.es>
2021-02-05 19:42:49 +0000

    Initial commit 💥

---
+++ src/main/java/es/leanmind/demo/InertiaApplication.java
@@ -0,0 +1,13 @@
+package es.leanmind.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InertiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InertiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/leanmind/demo/inertia/InertiaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.leanmind.demo.inertia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InertiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
